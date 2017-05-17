bba62ffd2194d571ff88425a0ffb7d19c91097bf
andres.cuervo <andres.cuervo@tecnilogica.com>
2017-05-17 18:27:59 +0200

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 
 // set up handlebars view engine
@@ -13,20 +14,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.render('home');
-    // res.type('text/plain');
-    // res.send('Meadowlark Travel');
 });
 app.get('/about', function(req, res){
-    var fortunes = [
-            "Conquer your fears or they will conquer you.",
-            "Rivers neeed springs.",
-            "Do not fear what you don't know.",
-            "You will have a pleasant surprise.",
-            "Whenever possible, keep it simple.",
-    ];
-
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
